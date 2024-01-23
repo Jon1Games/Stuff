630a50e471c59600b5981d331255daa2ab21b872
jonas <Jonas.saslona@mx.de>
2024-01-23 21:59:32 +0100

    Fixed name "stuf" -> "Stuff" Prepared Stuff.java

---
+++ src/main/java/de/jonas/stuff/Stuff.java
@@ -0,0 +1,54 @@
+package de.jonas.stuff;
+
+import de.jonas.stuff.commands.CalculatorCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+/*import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;*/
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPIBukkitConfig;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class Stuff extends JavaPlugin {
+
+    public static Stuff INSTANCE;
+    public static String PREFIX;
+    public Logger logger;
+
+    public void onLoad() {
+        this.logger = this.getLogger();
+
+        CommandAPI.onLoad(new CommandAPIBukkitConfig(this));
+        new CalculatorCommand();
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        INSTANCE = this;
+        PREFIX = "[Stuff] ";
+
+        //this.listener();
+
+        CommandAPI.onEnable();
+
+        logger.log(Level.INFO, "Activatet Plugin");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        CommandAPI.onDisable();
+
+        logger.log(Level.INFO,"Plugin deaktiviert.");
+    }
+
+    /*public void listener() {
+        PluginManager pm = Bukkit.getPluginManager();
+
+        pm.registerEvents(new ClickEvent(), this);
+    } */
+}
+
