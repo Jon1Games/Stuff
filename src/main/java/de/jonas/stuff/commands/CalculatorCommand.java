334556a2118904e20366102930b03d0fe71adfbe
jonas <Jonas.saslona@mx.de>
2024-01-23 21:53:56 +0100

    added Caclculator Command

---
+++ src/main/java/de/jonas/stuff/commands/CalculatorCommand.java
@@ -0,0 +1,64 @@
+package org.gaminglounge.skyblock.commands;
+
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.DoubleArgument;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+
+public class CalculatorCommand {
+
+    public CalculatorCommand() {
+        // Create our command
+        new CommandAPICommand("Rechner")
+                .withAliases("calc", "rechne")
+                .withSubcommand(new CommandAPICommand("+")
+                        .withArguments(new DoubleArgument("Zahl1"))
+                        .withArguments(new DoubleArgument("Zahl2"))
+                        .executesPlayer((player, args) -> {
+                            var mm = MiniMessage.miniMessage();
+                            double arg1 = (double) args.get("Zahl1");
+                            double arg2 = (double) args.get("Zahl2");
+                            double result = arg1 + arg2;
+
+                            Component msg = mm.deserialize("Das ergebnis von " + arg1 + " + " + arg2 + " ist: <green>" + result);
+                            player.sendMessage(msg);
+                        })
+                ).withSubcommand(new CommandAPICommand("-")
+                        .withArguments(new DoubleArgument("Zahl1"))
+                        .withArguments(new DoubleArgument("Zahl2"))
+                        .executesPlayer((player, args) -> {
+                            var mm = MiniMessage.miniMessage();
+                            double arg1 = (double) args.get("Zahl1");
+                            double arg2 = (double) args.get("Zahl2");
+                            double result = arg1 - arg2;
+
+                            Component msg = mm.deserialize("Das ergebnis von " + arg1 + " - " + arg2 + " ist: <green>" + result);
+                            player.sendMessage(msg);
+                        })
+                ).withSubcommand(new CommandAPICommand("*")
+                        .withArguments(new DoubleArgument("Zahl1"))
+                        .withArguments(new DoubleArgument("Zahl2"))
+                        .executesPlayer((player, args) -> {
+                            var mm = MiniMessage.miniMessage();
+                            double arg1 = (double) args.get("Zahl1");
+                            double arg2 = (double) args.get("Zahl2");
+                            double result = arg1 * arg2;
+
+                            Component msg = mm.deserialize("Das ergebnis von " + arg1 + " * " + arg2 + " ist: <green>" + result);
+                            player.sendMessage(msg);
+                        })
+                ).withSubcommand(new CommandAPICommand("/")
+                        .withArguments(new DoubleArgument("Zahl1"))
+                        .withArguments(new DoubleArgument("Zahl2"))
+                        .executesPlayer((player, args) -> {
+                            var mm = MiniMessage.miniMessage();
+                            double arg1 = (double) args.get("Zahl1");
+                            double arg2 = (double) args.get("Zahl2");
+                            double result = arg1 / arg2;
+
+                            Component msg = mm.deserialize("Das ergebnis von " + arg1 + " / " + arg2 + " ist: <green>" + result);
+                            player.sendMessage(msg);
+                        })
+                ).register();
+    }
+}
