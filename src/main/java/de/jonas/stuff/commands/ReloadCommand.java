cd59ad5632a2e4c853e4e418aa523901f3cb292f
jonas <jonas.saslona@gmx.de>
2024-05-19 23:21:22 +0200

    changes reload command

--- src/main/java/de/jonas/stuff/commands/ReloadCommand.java
+++ src/main/java/de/jonas/stuff/commands/ReloadCommand.java
@@ -9,26 +9,24 @@ public class ReloadCommand {
     Stuff stuff = Stuff.INSTANCE;
 
     public ReloadCommand() {
-        new CommandAPICommand("Stuff")
-                .withAliases("stuff")
-                .withSubcommand(new CommandAPICommand("reload")
-                        .withSubcommand(new CommandAPICommand("TeamDisplayName")
-                                .executes(((commandSender, args) -> {
-                                    Bukkit.getOnlinePlayers().forEach(pl -> {
-                                        stuff.teamDisplaynameSet.teams.remove(pl);
-                                        stuff.teamDisplaynameSet.removeTeam(stuff.teamDisplaynameSet.scoreboard, pl);
-                                        Bukkit.getOnlinePlayers().forEach(pl2 -> stuff.teamDisplaynameSet.
-                                                removeTeam(pl2.getScoreboard(), pl));
+        new CommandAPICommand("Stuff:Reload")
+                .withAliases("Stuff:reload", "stuff:Reload", "stuff:reload")
+                    .withSubcommand(new CommandAPICommand("TeamDisplayName")
+                            .executes(((commandSender, args) -> {
+                                Bukkit.getOnlinePlayers().forEach(pl -> {
+                                    stuff.teamDisplaynameSet.teams.remove(pl);
+                                    stuff.teamDisplaynameSet.removeTeam(stuff.teamDisplaynameSet.scoreboard, pl);
+                                    Bukkit.getOnlinePlayers().forEach(pl2 -> stuff.teamDisplaynameSet.
+                                            removeTeam(pl2.getScoreboard(), pl));
 
-                                        stuff.teamDisplaynameSet.onJoinPlayer(pl);
-                                    });
-                                }))
-                        )
-                        .withSubcommand(new CommandAPICommand("config")
-                                .executes(((commandSender, commandArguments) -> {
-                                    stuff.reloadConfig();
-                                }))
-                        )
+                                    stuff.teamDisplaynameSet.onJoinPlayer(pl);
+                                });
+                            }))
+                    )
+                    .withSubcommand(new CommandAPICommand("config")
+                            .executes(((commandSender, commandArguments) -> {
+                                stuff.reloadConfig();
+                            }))
                 )
                 .register();
     }
