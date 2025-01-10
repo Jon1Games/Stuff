eda658ac8a56d1aedc4438a188de3939edc11e06
jom1games <jonas@saslona.com>
2025-01-10 18:50:10 +0100

    fix double reload command suggestion

--- src/main/java/de/jonas/stuff/commands/ReloadCommand.java
+++ src/main/java/de/jonas/stuff/commands/ReloadCommand.java
@@ -15,33 +15,32 @@ public class ReloadCommand {
 
     public ReloadCommand() {
         new CommandAPICommand("Stuff:reload")
-                .withAliases("stuff:reload")
-                .withPermission(conf.getString("ReloadCommand.permission"))
-                    .withSubcommand(new CommandAPICommand("TeamDisplayName")
-                            .executes(((commandSender, args) -> {
-                                Bukkit.getOnlinePlayers().forEach(pl -> {
-                                    stuff.teamDisplaynameSet.teams.remove(pl);
-                                    stuff.teamDisplaynameSet.removeTeam(stuff.teamDisplaynameSet.scoreboard, pl);
-                                    Bukkit.getOnlinePlayers().forEach(pl2 -> stuff.teamDisplaynameSet.
-                                            removeTeam(pl2.getScoreboard(), pl));
+            .withPermission(conf.getString("ReloadCommand.permission"))
+                .withSubcommand(new CommandAPICommand("TeamDisplayName")
+                        .executes(((commandSender, args) -> {
+                            Bukkit.getOnlinePlayers().forEach(pl -> {
+                                stuff.teamDisplaynameSet.teams.remove(pl);
+                                stuff.teamDisplaynameSet.removeTeam(stuff.teamDisplaynameSet.scoreboard, pl);
+                                Bukkit.getOnlinePlayers().forEach(pl2 -> stuff.teamDisplaynameSet.
+                                        removeTeam(pl2.getScoreboard(), pl));
 
-                                    stuff.teamDisplaynameSet.onJoinPlayer(pl);
-                                    commandSender.sendMessage(mm.deserialize("[Stuff] Player names successfully reloaded"));
-                                });
-                            }))
-                    )
-                    .withSubcommand(new CommandAPICommand("config")
-                        .executes(((commandSender, commandArguments) -> {
-                            stuff.reloadConfig();
-                            commandSender.sendMessage(mm.deserialize("[Stuff] Config successfully reloaded"));
-                        })))
-                    .withSubcommand(new CommandAPICommand("timers")
-                        .executes(((commandSender, commandArguments) -> {
-                            stuff.reloadConfig();
-                            stuff.timerHandler.loadConfig();
-                            commandSender.sendMessage(mm.deserialize("[Stuff] Timers successfully reloaded"));
-                        })))
-                .register();
+                                stuff.teamDisplaynameSet.onJoinPlayer(pl);
+                                commandSender.sendMessage(mm.deserialize("[Stuff] Player names successfully reloaded"));
+                            });
+                        }))
+                )
+                .withSubcommand(new CommandAPICommand("config")
+                    .executes(((commandSender, commandArguments) -> {
+                        stuff.reloadConfig();
+                        commandSender.sendMessage(mm.deserialize("[Stuff] Config successfully reloaded"));
+                    })))
+                .withSubcommand(new CommandAPICommand("timers")
+                    .executes(((commandSender, commandArguments) -> {
+                        stuff.reloadConfig();
+                        stuff.timerHandler.loadConfig();
+                        commandSender.sendMessage(mm.deserialize("[Stuff] Timers successfully reloaded"));
+                    })))
+            .register();
     }
 
 
