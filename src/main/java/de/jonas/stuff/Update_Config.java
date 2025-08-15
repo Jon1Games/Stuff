fc10c36529fa3f8b904f90083f7e8bd63a247972
Jon1Games <118659471+Jon1Games@users.noreply.github.com>
2025-08-15 15:48:36 +0200

    update config presets for next release

--- src/main/java/de/jonas/stuff/Update_Config.java
+++ src/main/java/de/jonas/stuff/Update_Config.java
@@ -18,16 +18,16 @@ public class Update_Config {
                 config.set("CraftingRecipes.light.shaped", true);
                 config.set("CraftingRecipes.light.count", 2);
                 config.set("CraftingRecipes.light.publish", true);
-                config.set("CraftingRecipes.light.shape", new String[]{"XAX", "XBX", "XXX"});
-                config.set("CraftingRecipes.light.ingredients.A", "glass");
-                config.set("CraftingRecipes.light.ingredients.B", "torch");
+                config.set("CraftingRecipes.light.shape", new String[]{"A", "B"});
+                config.set("CraftingRecipes.light.ingredients.A", "GLASS");
+                config.set("CraftingRecipes.light.ingredients.B", "TORCH");
                 
                 // Add stone recipe
                 config.set("CraftingRecipes.stone.Enabled", false);
                 config.set("CraftingRecipes.stone.shaped", false);
                 config.set("CraftingRecipes.stone.publish", false);
                 config.set("CraftingRecipes.stone.count", 1);
-                config.set("CraftingRecipes.stone.ingredients", new String[]{"stone"});
+                config.set("CraftingRecipes.stone.ingredients", new String[]{"STONE"});
 
                 // update version
                 config.set("config", "2");
