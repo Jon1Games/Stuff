f4d9b3fdf1fe5f9d64d72fbefa290023eb121e2c
Jon1Games <118659471+Jon1Games@users.noreply.github.com>
2025-08-18 18:52:10 +0200

    update config

--- src/main/java/de/jonas/stuff/Update_Config.java
+++ src/main/java/de/jonas/stuff/Update_Config.java
@@ -14,20 +14,21 @@ public class Update_Config {
                 config.set("CraftingRecipes.Enabled", true);
                 
                 // Add light recipe
-                config.set("CraftingRecipes.light.Enabled", true);
-                config.set("CraftingRecipes.light.shaped", true);
-                config.set("CraftingRecipes.light.count", 2);
-                config.set("CraftingRecipes.light.publish", true);
-                config.set("CraftingRecipes.light.shape", new String[]{"XAX", "XBX", "XXX"});
-                config.set("CraftingRecipes.light.ingredients.A", "glass");
-                config.set("CraftingRecipes.light.ingredients.B", "torch");
-                
+                config.set("CraftingRecipes.LIGHT.Enabled", true);
+                config.set("CraftingRecipes.LIGHT.shaped", true);
+                config.set("CraftingRecipes.LIGHT.count", 1);
+                config.set("CraftingRecipes.LIGHT.publish", true);
+                config.set("CraftingRecipes.LIGHT.shape", new String[]{"'' A '", "'ABA'", "' C '"});
+                config.set("CraftingRecipes.LIGHT.ingredients.A", "GLASS_PANE");
+                config.set("CraftingRecipes.LIGHT.ingredients.B", "COAL");
+                config.set("CraftingRecipes.LIGHT.ingredients.C", "COPPER_INGOT");
+
                 // Add stone recipe
-                config.set("CraftingRecipes.stone.Enabled", false);
-                config.set("CraftingRecipes.stone.shaped", false);
-                config.set("CraftingRecipes.stone.publish", false);
-                config.set("CraftingRecipes.stone.count", 1);
-                config.set("CraftingRecipes.stone.ingredients", new String[]{"stone"});
+                config.set("CraftingRecipes.STONE.Enabled", false);
+                config.set("CraftingRecipes.STONE.shaped", false);
+                config.set("CraftingRecipes.STONE.publish", false);
+                config.set("CraftingRecipes.STONE.count", 1);
+                config.set("CraftingRecipes.STONE.ingredients", new String[]{"STONE"});
 
                 // update version
                 config.set("config", "2");
